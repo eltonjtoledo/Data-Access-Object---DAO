cc2b9104e4d3c57a57b65fe89d747343740523cf
eltonjtoledo <eltonjtoledo@gmail.com>
2018-12-10 11:23:24 -0200

    Foi alterado o arquivo index.php, adicionando titulo a pagina

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title> pagina Inicial</title>
     </head>
     <body>
         <?php
         require_once './config.php';
         $sql = new sql();
-        
+       
         $usuario = $sql->select('SELECT * FROM tb_usuarios');
         echo json_encode($usuario);       
         ?>
