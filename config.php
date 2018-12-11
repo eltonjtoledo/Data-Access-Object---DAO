54f4e734cbd2e18dd0115b290a1bcaf49cfe428b
eltonjtoledo <eltonjtoledo@gmail.com>
2018-12-11 00:18:15 -0200

    Foi feito a criação da classe Usuario.php

--- config.php
+++ config.php
@@ -3,6 +3,5 @@ spl_autoload_register(function ($nameClass) {
    $filename = 'class'.DIRECTORY_SEPARATOR.$nameClass.'.php';
    if(file_exists($filename)){
        include_once $filename;
-       echo $filename.'</br>';
    }        
 });
--- index.php
+++ index.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Pagina Inicial</title>
-    </head>
-    <body>
-        <?php
-        require_once './config.php';
-        $sql = new sql();
-       
-        $usuario = $sql->select('SELECT * FROM tb_usuarios');
-        echo json_encode($usuario);       
-        ?>
-    </body>
-</html>
+<?php
+
+require_once './config.php';
+$user = new Usuario();
+$user->loadById('5');
+echo $user;
+?>
