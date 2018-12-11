ce755fda2a125e03917abfe5242f5c433c36eea3
eltonjtoledo <eltonjtoledo@gmail.com>
2018-12-11 09:10:35 -0200

    adicionado na classe Usuarios.php os metodos para listar dados completos, com pesquisa de palavras e com validação (login e senha)

--- class/Usuario.php
+++ class/Usuario.php
@@ -61,12 +61,45 @@ class Usuario {
         }
     }
     
+    public static function getList() {
+         $sql = new sql();
+         return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+    }
+    
+    public static function searchList($login) {
+        $sql = new sql();
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :login ORDER BY deslogin",array(
+           ":login"=> '%'.$login.'%'
+        ));
+    }   
+    
+    public function logar($login, $password) {
+        $sql = new sql();
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :login AND dessenha = :pass", array(
+            ":login" => $login,
+            ":pass" => $password
+        ));
+        
+        if(count($results) > 0){
+           $row = $results[0];
+
+           $this->setId($row['idusuario']);
+           $this->setlogin($row['deslogin']);
+           $this->setsenha($row['dessenha']);
+           
+           $dt = new DateTime($row['dtcadastro']);
+           $this->setdtcadastro($dt->format('d-m-Y'));
+        }else{
+            throw new Exception("Login e/ou Senha invalidos, Verifique se você digitou corretamente!");
+        }
+    }
+    
     public function __toString() {
         return json_encode(array(
                'idusuario' => $this->getId(),
                'deslogin' => $this->getlogin(),
                'dessenha' => $this->getsenha(),
                'dtcadastro' => $this->getdtcadastro()
-           ),JSON_PRETTY_PRINT);
+           ));
     }
 }
--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
-
 require_once './config.php';
-$user = new Usuario();
-$user->loadById('5');
-echo $user;
+//Carrega um usuario ppelo seu ID
+//$user = new Usuario();
+//$user->loadById('5');
+//echo $user;
+
+//Carrega uma lista com todos os Usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de Usuarios esquisando por texto informado
+//$search = Usuario::searchList('el');
+//echo json_encode($search);
+
+//Carrega um usuario validando com login e senha
+//$login = new Usuario();
+//$login->logar('elton', '6624');
+//echo $login;
+
